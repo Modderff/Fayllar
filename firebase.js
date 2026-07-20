2794329f260caa4e526f8fc6f8e75004266a5770
Modderff <moddercha@gmail.com>
2026-07-21 00:22:53 +0500

    Update and rename script.js to firebase.js

---
+++ firebase.js
@@ -0,0 +1,72 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  get,
+  runTransaction
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBagTt_GKxzKzUiIj8dIVZu-gPM9-EHDUo",
+  authDomain: "planning-with-ai-7f474.firebaseapp.com",
+  databaseURL: "https://planning-with-ai-7f474-default-rtdb.firebaseio.com",
+  projectId: "planning-with-ai-7f474",
+  storageBucket: "planning-with-ai-7f474.firebasestorage.app",
+  messagingSenderId: "730296215902",
+  appId: "1:730296215902:web:ef0487d2ac628724e947cf"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+window.getFirebaseKey = async function () {
+
+    const oldKey = localStorage.getItem("myKey");
+
+    if (oldKey) {
+        document.getElementById("codeText").textContent = oldKey;
+        return;
+    }
+
+    const snap = await get(ref(db, "keys"));
+
+    if (!snap.exists()) {
+        document.getElementById("codeText").textContent = "KEY TOPILMADI";
+        return;
+    }
+
+    const keys = snap.val();
+
+    for (const id in keys) {
+
+        const result = await runTransaction(
+            ref(db, "keys/" + id),
+            (data) => {
+
+                if (data == null) return data;
+
+                if (data.used === false) {
+                    data.used = true;
+                    return data;
+                }
+
+                return;
+
+            }
+        );
+
+        if (result.committed) {
+
+            const key = result.snapshot.val().key;
+
+            localStorage.setItem("myKey", key);
+
+            document.getElementById("codeText").textContent = key;
+
+            return;
+        }
+    }
+
+    document.getElementById("codeText").textContent = "KEY QOLMAGAN";
+
+};
